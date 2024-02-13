126798d6210e83e1f53e7768f62f528bd6fa7d18
yaroslav <yaroslav_filippov_2017@mail.ru>
2024-02-13 10:45:15 +0100

    feat: added plot

--- time-summary.py
+++ time-summary.py
@@ -1,9 +1,12 @@
 import sys
 
+import numpy as np
+from matplotlib import pyplot as plt
+
 
 def prettify_time(task, time):
     tmp = task + ':'
-    if time > 60:
+    if time >= 60:
         tmp += f" {time // 60}h"
     if time % 60 != 0:
         tmp += f" {time % 60}m"
@@ -11,10 +14,17 @@ def prettify_time(task, time):
 
 
 if __name__ == '__main__':
+
     time_cost = int(input("Enter time cost: "))
     inp = sys.stdin.readlines()
     summary = 0
-    for line in inp:
+
+    graph_x = []
+    graph_y = []
+
+    day_summary = 0
+    for line_id in range(len(inp)):
+        line = inp[line_id]
         if line.find(':') != -1:
             tmp_minutes = 0
             task, time = line.split(':', 1)
@@ -30,7 +40,26 @@ if __name__ == '__main__':
 
             print(prettify_time(task, tmp_minutes))
             summary += tmp_minutes
+            day_summary += tmp_minutes
+        else:
+            graph_x.append(line.rstrip().split('.')[0].lstrip())
+            graph_y.append(day_summary / 60)
+            day_summary = 0
+
 
     print()
     print(f"Hours: {summary // 60}, Minutes: {summary % 60}")
     print(f"Sum: {round(summary * time_cost / 60, 2)}")
+
+    graph_y.append(day_summary / 60)
+    graph_y = graph_y[1:]
+
+    plt.axhline(y=sum(graph_y) / len(graph_x), color='r')
+
+    plt.yticks(np.arange(0, max(graph_y) + 1, 0.5))
+
+    plt.plot(graph_x, graph_y)
+
+    plt.grid()
+
+    plt.show()
